5c7d51620ba57eb61e0eb1298fb77c9a162d38f2
Lawrence Chan <lawrence.c@hotmail.co.uk>
2022-08-31 09:55:56 -0700

    Create a fetcher app that takes in a URL and local file path from the user input, and pulls the document from the url and copy onto the local file path. Includes extra utility that asks the user if they want to overwrite the file or not

---
+++ fetcher.js
@@ -0,0 +1,37 @@
+const url = process.argv[2];
+const localPath = process.argv[3];
+const readline = require("readline")
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+const fs = require("fs");
+
+const request = require('request');
+request(url, (error, response, body) => {
+  if (fs.existsSync(localPath)){
+    rl.question("File already exists. Would you like to overwrrite it?\n-y Yes\n-n No\n", (answer) => {
+      if (answer === "n") {
+        process.exit();
+      } else if (answer === "y") {
+        fs.promises.writeFile(localPath, body, "utf8").then(() => {
+          stats = fs.statSync(localPath)
+          console.log(`Downloaded ${stats.size} bytes to ${localPath}`)
+          process.exit();
+        }).catch(err => {
+          console.log(err);
+          process.exit();
+        })
+      }});
+  } else {
+    fs.promises.writeFile(localPath, body, "utf8").then(() => {
+      stats = fs.statSync(localPath)
+      console.log(`Downloaded ${stats.size} bytes to ${localPath}`)
+      process.exit();
+    }).catch(err => {
+      console.log(err);
+      process.exit();
+    })
+  }
+});
+
